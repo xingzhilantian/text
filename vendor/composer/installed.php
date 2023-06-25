e73bd5a98db12a0e18247b8ba2776c3baf631b5c
XingZai <43944437+xingzhilantian@users.noreply.github.com>
2023-06-25 03:24:25 +0000

    Pending changes exported from your codespace

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '365af01f88970b4a9e52d5849b80835b1f6bbc65',
+    'name' => 'xingzhilantian/testone',
+  ),
+  'versions' => 
+  array (
+    'xingzhilantian/testone' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '365af01f88970b4a9e52d5849b80835b1f6bbc65',
+    ),
+  ),
+);
